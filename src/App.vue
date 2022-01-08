6bcee182940d5e279f0f900065cb3b39322a6a88
mauricio <mauricio.vera.r7@gmail.com>
2022-01-08 12:44:45 -0300

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
+
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <b-navbar>
+          <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+              <img src="./assets/logo.png" />
+            </b-navbar-item> 
+          </template>
+          <template slot="start">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }"> Inicio 
+            </b-navbar-item>
+            <b-navbar-dropdown label="Productos">
+              <b-navbar-item tag="router-link" :to="{ path: '/p/electronica' }">
+                Electrónica
+              </b-navbar-item>
+              <b-navbar-item tag="router-link" :to="{ path: '/p/decoracion' }">
+                Decoración
+              </b-navbar-item>
+              <b-navbar-item tag="router-link" :to="{ path: '/p/computacion' }">
+                Computación
+              </b-navbar-item>
+            </b-navbar-dropdown>
+            <b-navbar-item tag="router-link" :to="{ path: '/medios-de-pago' }">
+              Medios de pago
+            </b-navbar-item>
+          </template>
+          <template slot="end">
+            <b-navbar-item tag="div">
+              <div class="buttons">
+                <a class="button is-light">
+                  <strong>Regístrate</strong>
+                </a>
+                <a class="button"> Ingresa               
+                </a>
+              </div>
+            </b-navbar-item>
+          </template>
+      </b-navbar>
+      <router-view></router-view>
   </div>
 </template>
+
+
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+/*import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -9,4 +9,19 @@ new Vue({
   router,
   store,
   render: h => h(App)
+}).$mount('#app')
+*/
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+//import './registerServiceWorker'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+Vue.use(Buefy)
+Vue.config.productionTip = false
+new Vue({
+router,
+store,
+render: h => h(App)
 }).$mount('#app')
